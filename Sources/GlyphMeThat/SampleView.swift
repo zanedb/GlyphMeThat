e8617bd8c8c2cc317702de856f669de26b9f22fc
Aether <64797587+Aeastr@users.noreply.github.com>
2025-04-23 02:50:42 +0100

    Update SampleView.swift

--- Sources/GlyphMeThat/SampleView.swift
+++ Sources/GlyphMeThat/SampleView.swift
@@ -137,36 +137,3 @@ struct MyCustomGlyphEditorStyle: GlyphEditorStyle {
         }
     }
 }
-
-/// A view controller representable for picking an image from the photo library.
-private struct ImagePicker: UIViewControllerRepresentable {
-    @Binding var image: UIImage?
-
-    func makeCoordinator() -> Coordinator { Coordinator(self) }
-
-    func makeUIViewController(context: Context) -> UIImagePickerController {
-        let picker = UIImagePickerController()
-        picker.delegate = context.coordinator
-        picker.sourceType = .photoLibrary
-        return picker
-    }
-
-    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
-
-    final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-        let parent: ImagePicker
-        init(_ parent: ImagePicker) { self.parent = parent }
-
-        func imagePickerController(_ picker: UIImagePickerController,
-                                   didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let uiImage = info[.originalImage] as? UIImage {
-                parent.image = uiImage
-            }
-            picker.dismiss(animated: true)
-        }
-
-        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            picker.dismiss(animated: true)
-        }
-    }
-}
